d2718fe4182e2dbf8372030a390a1816d1982ac0
wypoulos <73571414+wypoulos@users.noreply.github.com>
2020-12-15 19:02:45 -0700

    Add files via upload

---
+++ hw0.cc
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Three, one to get the Gucci brand ladder,\n";
+	cout << "one to screw the bulb in and one to ramble\n";
+	cout << "about how they will take over their parents company after college.\n";
+	return 0;
+}
